884edca9be022c87898c6807130fd3dd868b85ef
aoki-cloud <takosukeaoki@icloud.com>
2024-11-10 14:18:22 +0900

    テスト編集

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Admin;
@@ -18,7 +19,7 @@ class UserTest extends TestCase
 
     /*会員一覧ページ*/
     /*未ログインのユーザーは管理者側の会員一覧ページにアクセスできない*/
-    public function test_guest_cannot_access_admin_users_index()
+    public function test_guest_cannot_access_admin_users_index(): void
     {
         $response = $this->get(route('admin.users.index'));
 
@@ -38,12 +39,10 @@ class UserTest extends TestCase
     /*ログイン済みの管理者は管理者側の会員一覧ページにアクセスできる*/
     public function test_adminUser_can_access_admin_users_index()
     {
-        $adminUser = admin::factory()->create();
-
         $admin = new Admin();
-        $adminUser->email = 'admin@example.com';
-        $adminUser->password = Hash::make('nagoyameshi');
-        $adminUser->save();
+        $admin->email = 'admin@example.com';
+        $admin->password = Hash::make('nagoyameshi');
+        $admin->save();
 
         $response = $this->actingAs($adminUser, 'admin')->get(route('admin.users.index'));
 
@@ -52,7 +51,7 @@ class UserTest extends TestCase
 
     /*会員詳細ページ*/
     /*未ログインのユーザーは管理者側の会員詳細ページにアクセスできない*/
-    public function test_guest_cannnot_access_admin_users_show()
+    public function test_guest_cannnot_access_admin_users_show(): void
     {
         $user = User::factory()->create();
 
@@ -74,12 +73,10 @@ class UserTest extends TestCase
     /*ログイン済みの管理者は管理者側の会員詳細ページにアクセスできる*/
     public function test_adminUser_can_access_admin_usrs_show()
     {
-        $user = User::factory()->create();
-
-        $adminUser = new Admin();
-        $adminUser->email = 'admin@example.com';
-        $adminUser->password = Hash::make('nagoyameshi');
-        $adminUser->save();
+        $admin = new Admin();
+        $admin->email = 'admin@example.com';
+        $admin->password = Hash::make('nagoyameshi');
+        $admin->save();
 
         $adminUser = Admin::factory()->create();
 
